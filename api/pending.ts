b29fd5950cf3a92cbeaae30ec4fde7051928ca30
lajsijshau <73123516+lajsijshau@users.noreply.github.com>
2021-02-15 14:30:57 +0200

    Update pending.ts

--- api/pending.ts
+++ api/pending.ts
@@ -10,7 +10,7 @@ const getBalanceNumber = (balance: any, decimals = 18) => {
 };
 
 const pending = async (pid: number, address: string) => {
-  const chef = getContract(chefABI, "0x73feaa1eE314F8c655E354234017bE2193C9E24E");
+  const chef = getContract(chefABI, "");
   const pending = await chef.methods.pendingCake(pid, address).call();
   const poolInfo = await chef.methods.poolInfo(pid).call();
   return {
@@ -20,7 +20,7 @@ const pending = async (pid: number, address: string) => {
 };
 
 export default async (req: NowRequest, res: NowResponse): Promise<void> => {
-  const { address = "0x0F9399FC81DaC77908A2Dde54Bb87Ee2D17a3373", pid = "1" } = req.query;
+  const { address = "", pid = "1" } = req.query;
   if (Array.isArray(pid)) {
     res.status(400).send({ error: "Parameter Incorrect" });
   } else {
